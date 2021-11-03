c9d740fdbb91a559e079fe8e1c91ee18093bd17d
Rolando <riverarolando71@gmail.com>
2021-11-02 22:37:51 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-// EFECTO SCROLL DEL MENU DE NAVEGACION
-window.addEventListener("scroll",function(){
-    scroll()
+// *************************Menu de Navegación**********************
+document.addEventListener("scroll",function(){
+scrol()
 })
-let positionTop=window.pageYOffset;
+let scrollTop=window.pageYOffset;
+function scrol(){
+    let scrollAct=window.pageYOffset;
+    if(scrollTop<scrollAct){
+        // document.querySelector(".main").style.top="-200px";
+        document.querySelector(".main").style.height="0px";
+        document.querySelector(".main").style.overflow="hidden";
+        document.querySelector(".main").style.padding="0px";
 
-function scroll(){
-    let positionActual=window.pageYOffset;
 
-    if(positionTop>positionActual){
-        document.querySelector(".main").style.top="0";
     }else{
-        document.querySelector(".main").style.top="-200px";
+        // document.querySelector(".main").style.top="0px";
+        document.querySelector(".main").style.height="100px";
+        // document.querySelector(".main").style.overflow="visible";
+
+
     }
-    positionTop=positionActual
+    scrollTop=scrollAct
 }
 
-// MOSTRAR OCULTAR MENU UL
+// ******************************Mostrar menu en pantalla pequeña**************************
+let bars=document.querySelector(".bars");
+let cerrar=document.querySelector(".cerrar");
 
-let menu=document.querySelector(".menu");
-let cerrar=document.querySelector(".close");
-let ul=document.querySelector(".ul");
-let cx=document.querySelector(".cx");
-
-menu.addEventListener("click",function(){
-    ul.classList.toggle("mostrar_ul");
-    menu.style.display="none"
-});
+bars.addEventListener("click",function(){
+    document.querySelector(".ul").classList.toggle("ver_ul");
+    bars.style.display="none";
+    document.querySelector(".car").style.display="none"
+})
 cerrar.addEventListener("click",function(){
-    ul.classList.toggle("mostrar_ul");
-    menu.style.display="block"
+    document.querySelector(".ul").classList.toggle("ver_ul");
+    bars.style.display="block";
+    document.querySelector(".car").style.display="block"  
 })
-
-ul.addEventListener("click",function(u){
-
-    cerr(u.target)
-
-})
-function cerr(c){
-    if(c.className=="cx"){
-        ul.classList.toggle("mostrar_ul");
-        menu.style.display="block"
-    }
-}
-
-
-
